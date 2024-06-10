5ff8fd09043496ffdb918e3523f296b4d336a002
Agus <agustinferres4403@gmail.com>
2024-06-10 12:15:36 -0300

    node and path generation

--- Assets/Scripts/BoardGenerator.cs
+++ Assets/Scripts/BoardGenerator.cs
@@ -1,116 +1,114 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Utils;
 
 public class BoardGenerator : MonoBehaviour
 {
-    [SerializeField] private Tile hexTile;
-    [SerializeField] private Node node;
-    [SerializeField] private int gridWidth;
-    [SerializeField] private int gridHeight;
-    [SerializeField] private Camera camera;
-    private readonly float hexSize = 1.0f;
-    private Dictionary<Vector3, int> nodes = new Dictionary<Vector3, int>();
-    private List<int> nodesIndex = new List<int>();
+    public int gridHeight;
+    public int gridWidth;
+    public float hexSize = 1f;
+    public float pathOffset = .85f;
+    
+    public GameObject tiles;
+    public GameObject nodes;
+    public GameObject paths;
+    
+    public Tile tilePrefab;
+    public Node nodePrefab;
+    public Path pathPrefab;
+
+    private List<(float, float)> nodePositions = new();
+    private List<(float, float)> pathPositions = new();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int i = 0; // Start i from 0
+        GenerateGrid();
+    }
 
-        camera.transform.position = new Vector3(gridWidth * hexSize * .6f, (gridWidth + gridHeight) * 2, -gridHeight * hexSize * Mathf.Sqrt(3) * .22f);
-        camera.transform.rotation = Quaternion.Euler(90, -30, 0);
-
+    void GenerateGrid()
+    {
+        
         gridHeight = gridHeight % 2 != 0 ? gridHeight : gridHeight - 1; // make sure gridHeight is odd
-        gridHeight = gridHeight > gridWidth * 2 ? gridWidth * 2 - 1 : gridHeight; // make sure gridHeight is less than gridLength * 2
+        gridHeight =
+            gridHeight > gridWidth * 2
+                ? gridWidth * 2 - 1
+                : gridHeight; // make sure gridHeight is less than gridLength * 2
 
+        var initialLength = gridWidth - Mathf.FloorToInt(gridHeight / 2f);
+        var tileId = 0;
 
-        int initialLength = gridWidth - Mathf.FloorToInt(gridHeight / 2);
-        for (int j = 0; j < gridHeight; j++)
+        for (var j = 0; j < gridHeight; j++)
         {
-            int actualLength = ActualLength(initialLength, j);
-            for (int k = 0; k < actualLength; k++)
+            var actualLength = GridUtils.ActualLength(initialLength, j, gridHeight, gridWidth);
+            for (var k = 0; k < actualLength; k++)
             {
-                var hex = Instantiate(hexTile);
-                hex.SetText(i.ToString());
-                hex.transform.position = HexToPixel(new Vector2(ActualPosition(k, j), -j));
-
-                int value = i * 2 + j;
-                GenerateTopNodes(hex, value, nodes); // Generate top nodes
-                int value2 = j < gridHeight / 2 ? value + actualLength * 2 + 2 : value * 2 - (i + (k + 1));
-                GenerateBottomNodes(hex, value2, nodes); // Generate bottom node
-                i++;
+                var hexPosition = CalculateHexPosition(GridUtils.ActualPosition(k, j, gridHeight), -j);
+                var tile = Instantiate(tilePrefab, hexPosition, Quaternion.identity, tiles.transform);
+                tile.SetText($"{tileId}");
+                
+                GenerateNodes(hexPosition);
+                GeneratePaths(hexPosition);
+                
+                tileId++;
             }
         }
-
-        foreach (var node in nodes)
-        {
-            var n = Instantiate(this.node);
-            n.transform.position = node.Key;
-            n.SetText(node.Value.ToString());
-        }
     }
 
-    // Update is called once per frame
-    void Update()
+    Vector3 CalculateHexPosition(int hexX, int hexY)
     {
+        var hex = new Vector2(hexX, hexY);
 
-    }
-
-    Vector3 HexToPixel(Vector2 hex)
-    {
         var x = hexSize * 3 / 2 * hex.x;
         var z = hexSize * Mathf.Sqrt(3) * (hex.y + hex.x / 2);
         return new Vector3(x, 0, z);
     }
 
-    int ActualLength(int initialLength, int j)
+    private void GenerateNodes(Vector3 hexPosition)
     {
-        return j <= gridHeight / 2 ? initialLength + j : gridWidth - (j - gridHeight / 2);
-    }
-
-    int ActualPosition(int k, int j)
-    {
-        return j <= gridHeight / 2 ? k : k + (j - gridHeight / 2);
-    }
-
-    void GenerateTopNodes(Tile hexTile, int value, Dictionary<Vector3, int> nodes)
-    {
-        // node 1
-        Vector3 node1 = hexTile.transform.position - new Vector3(hexSize, 0, 0);
-        CheckAndAddNode(node1, value, nodes);
-
-        // node 2
-        Vector3 node2 = hexTile.transform.position - new Vector3(hexSize / 2, 0, hexSize * Mathf.Sqrt(3) / -2);
-        CheckAndAddNode(node2, value + 1, nodes);
-
-        // node 3
-        Vector3 node3 = hexTile.transform.position + new Vector3(hexSize / 2, 0, hexSize * Mathf.Sqrt(3) / 2);
-        CheckAndAddNode(node3, value + 2, nodes);
-    }
-
-    void GenerateBottomNodes(Tile hexTile, int value, Dictionary<Vector3, int> nodes)
-    {
-        // node 4
-        Vector3 node4 = hexTile.transform.position - new Vector3(hexSize / 2, 0, hexSize * Mathf.Sqrt(3) / 2);
-        CheckAndAddNode(node4, value, nodes);
-
-        // node 5
-        Vector3 node5 = hexTile.transform.position + new Vector3(hexSize / 2, 0, hexSize * Mathf.Sqrt(3) / -2);
-        CheckAndAddNode(node5, value + 1, nodes);
-
-        // node 6
-        Vector3 node6 = hexTile.transform.position + new Vector3(hexSize, 0, 0);
-        CheckAndAddNode(node6, value + 2, nodes);
-    }
-
-    void CheckAndAddNode(Vector3 node, int value, Dictionary<Vector3, int> nodes)
-    {
-        if (!nodes.ContainsKey(node) && !nodesIndex.Contains(value))
+        const float angleDeg = 60f;
+        for (var i = 0; i < 6; i++)
         {
-            nodes.Add(node, value);
-            nodesIndex.Add(value);
+            var angleRad = Mathf.Deg2Rad * (angleDeg * i);
+            var position = new Vector3(
+                hexPosition.x + hexSize * Mathf.Cos(angleRad),
+                0,
+                hexPosition.z + hexSize * Mathf.Sin(angleRad)
+            );
+            
+            var roundedPosition = (Mathf.Round(position.x * 100) / 100, Mathf.Round(position.z * 100) / 100);
+
+            if (!nodePositions.Contains(roundedPosition))
+            {
+                var node =Instantiate(nodePrefab, position, Quaternion.identity, nodes.transform);
+                node.SetText($"{roundedPosition}");
+                nodePositions.Add(roundedPosition);
+            }
         }
     }
 
+    private void GeneratePaths(Vector3 hexPosition)
+    {
+        const float angleDeg = 60f;
+        for (var i = 0; i < 6; i++)
+        {
+            var angleRad = Mathf.Deg2Rad * (angleDeg * i + 30);
+            var position = new Vector3(
+                hexPosition.x + hexSize * Mathf.Cos(angleRad) * pathOffset,
+                    0,
+                hexPosition.z + hexSize * Mathf.Sin(angleRad) * pathOffset
+            );
+            
+            var roundedPosition = (Mathf.Round(position.x * 100) / 100, Mathf.Round(position.z * 100) / 100);
+            
+            if (!pathPositions.Contains(roundedPosition))
+            {
+                var path = Instantiate(pathPrefab, position, Quaternion.identity, paths.transform);
+                pathPositions.Add(roundedPosition);
+                
+                path.transform.LookAt(hexPosition);
+                path.transform.position += new Vector3(0, .1f, 0);
+            }
+        }
+    }
 }
---
+++ Assets/Utils/GridUtils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public static class GridUtils
+    {
+        public static int ActualLength(int initialLength, int j, int gridHeight, int gridWidth)
+        {
+            return j <= gridHeight / 2 ? initialLength + j : gridWidth - (j - gridHeight / 2);
+        }
+        
+        public static int ActualPosition(int initialPosition, int j, int gridHeight)
+        {
+            return j <= gridHeight / 2 ? initialPosition : initialPosition + (j - gridHeight / 2);
+        }
+    }
+}
